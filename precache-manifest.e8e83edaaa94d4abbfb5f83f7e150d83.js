5b2f02f47695e9e19377393283d65f2387316e5a
rabindranath <connectnmwd@gmail.com>
2021-09-02 14:58:47 +0530

    Updates

---
+++ precache-manifest.e8e83edaaa94d4abbfb5f83f7e150d83.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3708eb68c01f748c4cc54bd16fcd27",
+    "url": "/Rabindranath894592/messenger-solar.git/index.html"
+  },
+  {
+    "revision": "abcf943982b093843dfa",
+    "url": "/Rabindranath894592/messenger-solar.git/static/css/2.f0d82c5f.chunk.css"
+  },
+  {
+    "revision": "b43fc73d89b4a1f43b21",
+    "url": "/Rabindranath894592/messenger-solar.git/static/css/main.7a029611.chunk.css"
+  },
+  {
+    "revision": "abcf943982b093843dfa",
+    "url": "/Rabindranath894592/messenger-solar.git/static/js/2.76f43d8a.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/Rabindranath894592/messenger-solar.git/static/js/2.76f43d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43fc73d89b4a1f43b21",
+    "url": "/Rabindranath894592/messenger-solar.git/static/js/main.2c7ad30d.chunk.js"
+  },
+  {
+    "revision": "4381d65d38c674de341b",
+    "url": "/Rabindranath894592/messenger-solar.git/static/js/runtime-main.c7547616.js"
+  },
+  {
+    "revision": "bd92888402a2e9d1c938cf0307ba0715",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/MessengerSolarBanner.bd928884.jpg"
+  },
+  {
+    "revision": "1551d39eefa35865c4b72be1bb27abbe",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/Solar-cap.1551d39e.jpg"
+  },
+  {
+    "revision": "b443a120eca85139767769117b61f991",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/Solar-helmet.b443a120.jpg"
+  },
+  {
+    "revision": "29dc0e93f6b1c361aa78b0f44719a105",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/avataar.29dc0e93.jpg"
+  },
+  {
+    "revision": "e644ef7a557633c484f6a7a980379759",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/enrolment-certificate.e644ef7a.jpg"
+  },
+  {
+    "revision": "bb4840f950ded073cf40c92346507911",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/home02.bb4840f9.png"
+  },
+  {
+    "revision": "9e77bdfab4887733f8a0debcb9cd3576",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/logo.9e77bdfa.png"
+  },
+  {
+    "revision": "b20f8be7e7a7f448b025e9e73d514e57",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/registration-certificate-updated.b20f8be7.jpeg"
+  },
+  {
+    "revision": "29150fe61c6b747b2fcd794cf1d7de16",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-air-purifier.29150fe6.jpg"
+  },
+  {
+    "revision": "1867a5f04fade0c5929515d026773841",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-auto.1867a5f0.jpg"
+  },
+  {
+    "revision": "87fcab108b3e6750c9bd9594c10bfaf8",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-e-rickshaw.87fcab10.jpg"
+  },
+  {
+    "revision": "f2dd08e64058e5d9e5d6ec257f76ccc0",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-fm-radio.f2dd08e6.jpg"
+  },
+  {
+    "revision": "b9c6de7e6f3916b3f9d414d6db85f352",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-home-light.b9c6de7e.jpg"
+  },
+  {
+    "revision": "cdf7e17c443f566923cb5e42c1ce08b4",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-multi-charger.cdf7e17c.jpg"
+  },
+  {
+    "revision": "6bcaa1085dee50a27d392841bc9f1aa9",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-panel.6bcaa108.jpg"
+  },
+  {
+    "revision": "4758f0fe05af521cd35ae83964fdeea0",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-power-bank.4758f0fe.jpg"
+  },
+  {
+    "revision": "18920ba0f23f34141fcefe9b26371347",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-pump.18920ba0.jpg"
+  },
+  {
+    "revision": "1c356c1147b9ff3cf7d53999563e77e2",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-salu.1c356c11.jpg"
+  },
+  {
+    "revision": "1e3e7f2a38e5696234fd2755128a8f8c",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-signal-light.1e3e7f2a.jpg"
+  },
+  {
+    "revision": "929633eaa48b5fc2565db91d58034088",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-submersible.929633ea.jpg"
+  },
+  {
+    "revision": "0a7d7b11ad4cd2b715205a7507ec3618",
+    "url": "/Rabindranath894592/messenger-solar.git/static/media/solar-water-purifier.0a7d7b11.jpg"
+  }
+]);
